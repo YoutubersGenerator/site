28a851c5b4b3acd4e3d14fbfd0a9dd9c741a5838
BenJ <benj.wagner@outlook.com>
2015-03-25 11:46:57 +0100

    footer effectif

--- custom.js
+++ custom.js
@@ -1,14 +1,18 @@
+var footer = document.getElementById("footer");
+
 $(document).ready(function(){
     $('#nav').click(function(){
         $('#bandeau').fadeOut("500");
         $('#nav').fadeOut("500");
         $('#return').fadeIn("500");
-	$('#footer').fadeIn("500");
+        jQuery(footer).css('display',"block");
+        $('#footer').fadeIn("slow");
     });
-    $('#return').click(function(){
+        $('#return').click(function(){
         $('#bandeau').fadeIn("500");
         $('#nav').fadeIn("500");
-	$('#return').fadeOut("500");
-	$('#footer').fadeOut("500");
+        $('#return').fadeOut("500");
+        jQuery(footer).css('display',"none")
+        $('#footer').fadeOut("500");
     });
 });
