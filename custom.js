90399313bfebe958cf2f52a5ef563ac213c9eea4
BenJ <benj.wagner@outlook.com>
2015-03-25 11:13:44 +0100

    bouton return et fade out du footer

--- custom.js
+++ custom.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     $('#nav').click(function(){
-        $('#bandeau').fadeOut("slow");
-        $('#nav').fadeOut("slow");
-        $('#return').fadeIn("slow");
+        $('#bandeau').fadeOut("500");
+        $('#nav').fadeOut("500");
+        $('#return').fadeIn("500");
+	$('#footer').fadeIn("500");
     });
     $('#return').click(function(){
-        $('#bandeau').fadeIn("slow");
-        $('#nav').fadeIn("slow");
+        $('#bandeau').fadeIn("500");
+        $('#nav').fadeIn("500");
+	$('#return').fadeOut("500");
+	$('#footer').fadeOut("500");
     });
 });
 
