47be786c5ea5a6b35df22f05c5e03be21530065a
BenJ <benj.wagner@outlook.com>
2015-05-06 16:45:25 +0200

    vamos a uploadar

--- envoie.php
+++ envoie.php
@@ -14,19 +14,23 @@
 		$files = $_FILES['file'];
 		for($i=0; $i<count($files); $i++){
 			if(isset($files['name'][$i])){
-				$name = substr($files['name'][$i], 0, 3).'_';
+				$name = substr($files['name'][$i], 0, 3);
+				$chemin = 'audio/'.$name.'.mp3';
 				$id = attr_id($name);
 				$req = $bdd->prepare('INSERT INTO ytb(nom, id) VALUES(:nom, :id)');
 				$req->execute(array(
 					'nom' => $name,
 					'id' => $id
 				));
+
+				var_dump($chemin);
+				move_uploaded_file($_FILES['file']['tmp_name'][$i], $chemin);
 			}
 		}
 	}
 	function attr_id($nom){
 		$l = 1;
-		while (file_exists('audio/'.substr($nom, 0, -1).'/'.$nom.$l.'.mp3')) {
+		while (file_exists('audio/'.$nom.'/'.$nom.'_'.$l.'.mp3')) {
 			$l++;
 		}
 		return $l;
